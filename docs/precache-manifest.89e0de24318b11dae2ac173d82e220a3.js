6e354338e88cee2062d09a8f72dafba8d26e4b70
Lyubov <spiridonova73l@gmail.com>
2020-04-18 21:31:58 +0400

    first build

---
+++ docs/precache-manifest.89e0de24318b11dae2ac173d82e220a3.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f65005c9d552c6a37e87a5171aa195",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9ddb4f85be8c41b753c6",
+    "url": "./static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d8d6dedb527811f03176",
+    "url": "./static/css/main.1fdd96a5.chunk.css"
+  },
+  {
+    "revision": "9ddb4f85be8c41b753c6",
+    "url": "./static/js/2.e3b04a97.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.e3b04a97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8d6dedb527811f03176",
+    "url": "./static/js/main.7e277b7f.chunk.js"
+  },
+  {
+    "revision": "c4e68bbbff9a03a03b6c",
+    "url": "./static/js/runtime-main.6adf5236.js"
+  },
+  {
+    "revision": "0a7e4540a7a1d6c923ee1f256833223c",
+    "url": "./static/media/AvenirNextCyr-Demi.0a7e4540.ttf"
+  },
+  {
+    "revision": "4e8ae957f6fda0424b36c5fefdff776f",
+    "url": "./static/media/AvenirNextCyr-Demi.4e8ae957.woff2"
+  },
+  {
+    "revision": "9b91858f016cfcf8511d67d0fc698ffe",
+    "url": "./static/media/AvenirNextCyr-Demi.9b91858f.woff"
+  },
+  {
+    "revision": "328795d12e7693f1753bc85b2d2eb70b",
+    "url": "./static/media/AvenirNextCyr-Medium.328795d1.ttf"
+  },
+  {
+    "revision": "dcbd5b12144ca5f6dd6cc56218ac0dcb",
+    "url": "./static/media/AvenirNextCyr-Medium.dcbd5b12.woff"
+  },
+  {
+    "revision": "ff32b7069d5803d265261b923fbeb048",
+    "url": "./static/media/AvenirNextCyr-Medium.ff32b706.woff2"
+  },
+  {
+    "revision": "2e6a650a3617babd27b04606ffc15c57",
+    "url": "./static/media/AvenirNextCyr-MediumItalic.2e6a650a.ttf"
+  },
+  {
+    "revision": "30d73dde11cebe1118d2377432e7a79d",
+    "url": "./static/media/AvenirNextCyr-MediumItalic.30d73dde.woff"
+  },
+  {
+    "revision": "d3f1fedc0eddf03a7d2b87231a779278",
+    "url": "./static/media/AvenirNextCyr-MediumItalic.d3f1fedc.woff2"
+  },
+  {
+    "revision": "7439b855d7c0b43f42d227b056d0c95d",
+    "url": "./static/media/AvenirNextCyr-Regular.7439b855.woff2"
+  },
+  {
+    "revision": "a50242c650fbc493fb36977b3584b874",
+    "url": "./static/media/AvenirNextCyr-Regular.a50242c6.woff"
+  },
+  {
+    "revision": "bfce8899dd80257d3efda57876884870",
+    "url": "./static/media/AvenirNextCyr-Regular.bfce8899.ttf"
+  }
+]);
